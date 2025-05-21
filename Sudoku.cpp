6b79009d74746b588acc86c8daf5de11fa8a9486
sonukumarsoni-2007 <sonusoni07229@gmail.com>
2025-05-21 20:07:02 +0530

    Add files via upload

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -1,80 +1,86 @@
-#include <stdio.h>
-#include <stdbool.h>
+#include <iostream>
+#include <vector>
 
-#define N 9
+using namespace std;
+
+const int N = 9;
 
 // Function to print the Sudoku grid
-void printGrid(int grid[N][N]) {
-    for (int row = 0; row < N; row++) {
-        for (int col = 0; col < N; col++) {
-            printf("%d ", grid[row][col]);
+void printGrid(const vector<vector<int>>& grid) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << grid[i][j] << " ";
         }
-        printf("\n");
+        cout << endl;
     }
 }
 
-// Function to check if it's safe to place num in grid[row][col]
-bool isSafe(int grid[N][N], int row, int col, int num) {
-    // Check row and column
+// Function to check if placing num in grid[row][col] is valid
+bool isSafe(const vector<vector<int>>& grid, int row, int col, int num) {
+    // Check if num is not in the current row and column
     for (int x = 0; x < N; x++) {
-        if (grid[row][x] == num || grid[x][col] == num)
+        if (grid[row][x] == num || grid[x][col] == num) {
             return false;
+        }
     }
 
-    // Check 3x3 subgrid
-    int startRow = row - row % 3;
-    int startCol = col - col % 3;
-
+    // Check if num is not in the current 3x3 subgrid
+    int startRow = row - row % 3, startCol = col - col % 3;
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            if (grid[i + startRow][j + startCol] == num)
+            if (grid[i + startRow][j + startCol] == num) {
                 return false;
+            }
         }
     }
 
     return true;
 }
 
-// Recursive function to solve the Sudoku puzzle
-bool solveSudoku(int grid[N][N]) {
+// Function to solve the Sudoku puzzle using backtracking
+bool solveSudoku(vector<vector<int>>& grid) {
     int row, col;
-    bool emptyCellFound = false;
+    bool emptyCell = false;
 
     // Find an empty cell
     for (row = 0; row < N; row++) {
         for (col = 0; col < N; col++) {
             if (grid[row][col] == 0) {
-                emptyCellFound = true;
+                emptyCell = true;
                 break;
             }
         }
-        if (emptyCellFound)
+        if (emptyCell) {
             break;
+        }
     }
 
-    // No empty cell left; puzzle solved
-    if (!emptyCellFound)
+    // If no empty cell is found, the puzzle is solved
+    if (!emptyCell) {
         return true;
+    }
 
     // Try digits 1 to 9
-    for (int num = 1; num <= 9; num++) {
+    for (int num = 1; num <= N; num++) {
         if (isSafe(grid, row, col, num)) {
             grid[row][col] = num;
 
-            // Recursively attempt to fill in the rest of the grid
-            if (solveSudoku(grid))
+            // Recursively attempt to solve the rest of the grid
+            if (solveSudoku(grid)) {
                 return true;
+            }
 
-            // Backtrack
+            // If placing num doesn't lead to a solution, reset the cell
             grid[row][col] = 0;
         }
     }
 
-    return false; // Trigger backtracking
+    // Trigger backtracking
+    return false;
 }
 
 int main() {
-    int grid[N][N] = {
+    vector<vector<int>> grid = {
         {5, 3, 0, 0, 7, 0, 0, 0, 0},
         {6, 0, 0, 1, 9, 5, 0, 0, 0},
         {0, 9, 8, 0, 0, 0, 0, 6, 0},
@@ -86,11 +92,14 @@ int main() {
         {0, 0, 0, 0, 8, 0, 0, 7, 9}
     };
 
+    cout << "Initial Sudoku Grid:" << endl;
+    printGrid(grid);
+
     if (solveSudoku(grid)) {
-        printf("Solved Sudoku:\n");
+        cout << "\nSolved Sudoku Grid:" << endl;
         printGrid(grid);
     } else {
-        printf("No solution exists.\n");
+        cout << "\nNo solution exists!" << endl;
     }
 
     return 0;
