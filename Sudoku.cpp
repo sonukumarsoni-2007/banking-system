1b86098fbc19e53a3dc934b4457988949ccd6944
sonukumarsoni-2007 <sonusoni07229@gmail.com>
2025-05-25 20:51:35 +0530

    Update Sudoku.cpp

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 const int N = 9;
 
-// Function to print the Sudoku grid
+
 void printGrid(const vector<vector<int>>& grid) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -15,16 +15,12 @@ void printGrid(const vector<vector<int>>& grid) {
     }
 }
 
-// Function to check if placing num in grid[row][col] is valid
 bool isSafe(const vector<vector<int>>& grid, int row, int col, int num) {
-    // Check if num is not in the current row and column
     for (int x = 0; x < N; x++) {
         if (grid[row][x] == num || grid[x][col] == num) {
             return false;
         }
     }
-
-    // Check if num is not in the current 3x3 subgrid
     int startRow = row - row % 3, startCol = col - col % 3;
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
@@ -33,16 +29,11 @@ bool isSafe(const vector<vector<int>>& grid, int row, int col, int num) {
             }
         }
     }
-
     return true;
 }
-
-// Function to solve the Sudoku puzzle using backtracking
 bool solveSudoku(vector<vector<int>>& grid) {
     int row, col;
     bool emptyCell = false;
-
-    // Find an empty cell
     for (row = 0; row < N; row++) {
         for (col = 0; col < N; col++) {
             if (grid[row][col] == 0) {
@@ -54,28 +45,18 @@ bool solveSudoku(vector<vector<int>>& grid) {
             break;
         }
     }
-
-    // If no empty cell is found, the puzzle is solved
     if (!emptyCell) {
         return true;
     }
-
-    // Try digits 1 to 9
     for (int num = 1; num <= N; num++) {
         if (isSafe(grid, row, col, num)) {
             grid[row][col] = num;
-
-            // Recursively attempt to solve the rest of the grid
             if (solveSudoku(grid)) {
                 return true;
             }
-
-            // If placing num doesn't lead to a solution, reset the cell
             grid[row][col] = 0;
         }
     }
-
-    // Trigger backtracking
     return false;
 }
 
